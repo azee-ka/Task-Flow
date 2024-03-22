dd08a66f995f098f8351bf7ef13a0d238d74678c
azee-ka <singhvir943@gmail.com>
2024-03-21 23:34:30 -0700

    Calendar UI Update

--- tf/src/components/calendar/Calendar.js
+++ tf/src/components/calendar/Calendar.js
@@ -44,69 +44,72 @@ function Calendar() {
   }
 
   return (
-    <div className='calendar-body'>
-      <div className='calendar-container'>
-        <div className='calendar-nav'>
-          <a
-            href='#monthly'
-            className={viewType === 'monthly' ? 'active' : ''}
-            onClick={(e) => {
-              e.preventDefault();
-              setViewType('monthly');
-            }}
-          >
-            Month
-          </a>
-          <a
-            href='#yearly'
-            className={viewType === 'yearly' ? 'active' : ''}
-            onClick={(e) => {
-              e.preventDefault();
-              setViewType('yearly');
-            }}
-          >
-            Year
-          </a>
-          <a
-            href='#weekly'
-            className={viewType === 'weekly' ? 'active' : ''}
-            onClick={(e) => {
-              e.preventDefault();
-              setViewType('weekly');
-            }}
-          >
-            Week
-          </a>
+    <div className='calendar-container'>
+      <h2>Calendar</h2>
+      <div className='calendar-body'>
+      <div className='calendar-nav'>
+            <a
+              href='#monthly'
+              className={viewType === 'monthly' ? 'active' : ''}
+              onClick={(e) => {
+                e.preventDefault();
+                setViewType('monthly');
+              }}
+            >
+              Month
+            </a>
+            <a
+              href='#yearly'
+              className={viewType === 'yearly' ? 'active' : ''}
+              onClick={(e) => {
+                e.preventDefault();
+                setViewType('yearly');
+              }}
+            >
+              Year
+            </a>
+            <a
+              href='#weekly'
+              className={viewType === 'weekly' ? 'active' : ''}
+              onClick={(e) => {
+                e.preventDefault();
+                setViewType('weekly');
+              }}
+            >
+              Week
+            </a>
 
-        </div>
-        <div className='calendar-header'>
-          {viewType === 'monthly' && (
-            <>
-              <h2>{date.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}</h2>
-            </>
-          )}
-          {viewType === 'weekly' && (
-            <>
-              <h2>
-                {date.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}&nbsp;&nbsp;&nbsp;
-                Week {getWeek(date)}
-              </h2>
-
-            </>
-          )}
-          {viewType === 'yearly' && (
-            <>
-              <h2>Year {date.toLocaleDateString('en-US', { year: 'numeric' })}</h2>
-            </>
-          )}
-          <div className='calendar-buttons'>
-            <>
-              <button onClick={viewType === 'monthly' ? prevMonth : viewType === 'weekly' ? prevWeek : prevYear}><FontAwesomeIcon icon={faChevronLeft} /></button>
-              <button onClick={viewType === 'monthly' ? nextMonth : viewType === 'weekly' ? nextWeek : nextYear}><FontAwesomeIcon icon={faChevronRight} /></button>
-            </>
           </div>
+        <div className='calendar-container-inner'>
+          <div className='calendar-header'>
+            {viewType === 'monthly' && (
+              <>
+                <h2>{date.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}</h2>
+              </>
+            )}
+            {viewType === 'weekly' && (
+              <>
+                <h2>
+                  {date.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}&nbsp;&nbsp;&nbsp;
+                  Week {getWeek(date)}
+                </h2>
+
+              </>
+            )}
+            {viewType === 'yearly' && (
+              <>
+                <h2>Year {date.toLocaleDateString('en-US', { year: 'numeric' })}</h2>
+              </>
+            )}
+            <div className='calendar-buttons'>
+              <>
+                <button onClick={viewType === 'monthly' ? prevMonth : viewType === 'weekly' ? prevWeek : prevYear}><FontAwesomeIcon icon={faChevronLeft} /></button>
+                <button onClick={viewType === 'monthly' ? nextMonth : viewType === 'weekly' ? nextWeek : nextYear}><FontAwesomeIcon icon={faChevronRight} /></button>
+              </>
+            </div>
+          </div>
+          {calendarCells}
         </div>
-        {calendarCells}
       </div>
     </div>
   );
